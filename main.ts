69aad66250f930ec6c9aad0fdda5cc696433a1da
Lúcia Abreu <26051893+luciabreu@users.noreply.github.com>
2020-11-20 00:15:21 +0000

    Version with steering mode and button mode with cicle recording v1

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
+WSJoyStick.onKey(KEY.F, function () {
+    if (!(steeringMode)) {
+        if (isRecording) {
+            pushToRecordingList(3)
+        }
+    }
+})
+function updateScreen () {
+    if (steeringMode) {
+        basic.showIcon(IconNames.Triangle)
+    } else {
+        if (isRecording) {
+            basic.showIcon(IconNames.Target)
+        } else if (isPlaying) {
+            basic.showLeds(`
+                . # . . .
+                . # # . .
+                . # # # .
+                . # # . .
+                . # . . .
+                `)
+        } else {
+            basic.showIcon(IconNames.SmallSquare)
+        }
+    }
+}
+function pushToRecordingList (instruction: number) {
+    for (let index = 0; index < recordingCycles; index++) {
+        recordingList.push(instruction)
+    }
+}
 function translateInstruction (instruction: number) {
     if (instruction == 0) {
         radio.sendValue("fs", 0)
@@ -11,34 +42,81 @@ function translateInstruction (instruction: number) {
         radio.sendValue("dir", 40)
     }
 }
+WSJoyStick.onKey(KEY.A, function () {
+    if (!(steeringMode)) {
+        if (isRecording) {
+            endRecording()
+        } else {
+            isRecording = true
+            isPlaying = false
+            recordingList = []
+        }
+        updateScreen()
+    }
+})
+WSJoyStick.onKey(KEY.E, function () {
+    if (!(steeringMode)) {
+        if (isRecording) {
+            pushToRecordingList(1)
+        }
+    }
+})
+function endRecording () {
+    isRecording = false
+    recordingList.push(0)
+}
+WSJoyStick.onKey(KEY.D, function () {
+    if (!(steeringMode)) {
+        if (isRecording) {
+            pushToRecordingList(4)
+        }
+    }
+})
+WSJoyStick.onKey(KEY.B, function () {
+    if (!(steeringMode)) {
+        if (isPlaying) {
+            isPlaying = false
+        } else {
+            endRecording()
+            isPlaying = true
+            playIndex = 0
+        }
+        updateScreen()
+    }
+})
 WSJoyStick.onKey(KEY.C, function () {
     steeringMode = !(steeringMode)
+    updateScreen()
 })
 let steeringSpeed = 0
-let isRecording = false
 let instruction = 0
-let lista: number[] = []
 let playIndex = 0
+let recordingList: number[] = []
 let isPlaying = false
+let isRecording = false
+let recordingCycles = 0
 let steeringMode = false
-basic.showIcon(IconNames.Diamond)
 steeringMode = false
 let isStopped = true
 let straightSpeed = 30
 let maxSteeringSpeed = 50
 let steeringDeadzone = 20
+recordingCycles = 30
 radio.setGroup(31)
 WSJoyStick.JoyStickInit()
+updateScreen()
 basic.forever(function () {
     if (!(steeringMode)) {
         if (isPlaying) {
-            if (playIndex >= lista.length) {
+            if (playIndex >= recordingList.length) {
                 isPlaying = false
+                updateScreen()
             } else {
-                instruction = lista[playIndex]
+                instruction = recordingList[playIndex]
                 playIndex += 1
+                translateInstruction(instruction)
             }
-        } else {
+        } else if (!(isRecording)) {
             if (WSJoyStick.Listen_Key(KEY.E)) {
                 instruction = 1
             } else if (WSJoyStick.Listen_Key(KEY.F)) {
@@ -48,34 +126,18 @@ basic.forever(function () {
             } else {
                 instruction = 0
             }
-        }
-        if (isRecording) {
-            lista.push(instruction)
-        } else {
             translateInstruction(instruction)
-        }
-        if (WSJoyStick.Listen_Key(KEY.A) && isRecording) {
-            isRecording = false
-        } else if (WSJoyStick.Listen_Key(KEY.A)) {
-            isRecording = true
-            isPlaying = false
-            lista = []
-        }
-        if (WSJoyStick.Listen_Key(KEY.B) && isPlaying) {
-            isPlaying = false
-        } else if (WSJoyStick.Listen_Key(KEY.B)) {
-            isPlaying = true
-            playIndex = 0
-            isRecording = false
         }
     }
 })
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.U)) {
         isStopped = false
+        updateScreen()
     }
     if (WSJoyStick.Listen_Dir(DIR.D)) {
         isStopped = true
+        updateScreen()
     }
 })
 basic.forever(function () {
@@ -88,7 +150,6 @@ basic.forever(function () {
             radio.sendValue("fs", 0)
         } else {
             radio.sendValue("fs", straightSpeed)
-            basic.showIcon(IconNames.Triangle)
         }
     }
 })
